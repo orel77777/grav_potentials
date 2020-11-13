6af2bba224b7ba4c88962d1c6a92d2ec7d91e5fe
ivan <ivan@debian.ivan>
2020-11-13 07:07:31 +0300

    13.11.2020(13)

---
+++ potentials/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+
+from .potentials import *
+
+name = 'potentials'
+version = '0.0.1'
---
+++ torus_plot/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+
+from .torus_plot import *
+
+name = 'torus_plot'
+version = '0.0.1'
